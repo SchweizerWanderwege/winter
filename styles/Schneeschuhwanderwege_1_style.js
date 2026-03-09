282931d4db48941923a84a9b491e8d38e380f1f4
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2026-03-09 13:37:51 +0100

    Add files via upload

--- styles/Schneeschuhwanderwege_1_style.js
+++ styles/Schneeschuhwanderwege_1_style.js
@@ -1,53 +1,95 @@
 var size = 0;
 var placement = 'point';
+
+
 function categories_Schneeschuhwanderwege_1(feature, value, size, resolution, labelText,
                        labelFont, labelFill, bufferColor, bufferWidth,
                        placement) {
-                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
-                switch(valueStr) {case '1':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(200,60,120,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 6.00}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    }),new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(140,10,80,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 8.0}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;
-case '0':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(170,10,120,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 6.0}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;}};
+
+    var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+
+    // Zoomstufen
+    if (resolution >= 20) {
+        widthInner = 4;
+        widthOuter = 0;
+    }
+    else if (resolution >= 5) {
+        widthInner = 4;
+        widthOuter = 12;
+    }
+    else if (resolution >= 1) {
+        widthInner = 5;
+        widthOuter = 20;
+    }
+
+    switch(valueStr) {
+
+        case '1':
+
+            return [
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(140,10,80,0.7)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthInner
+                    }),
+                    text: createTextStyle(feature, resolution, labelText, labelFont,
+                                          labelFill, placement, bufferColor,
+                                          bufferWidth)
+                }),
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(186,73,143,0.4)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthOuter
+                    })
+                })
+            ];
+
+        case '0':
+
+            return [
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(140,10,80,0.7)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthInner
+                    }),
+                    text: createTextStyle(feature, resolution, labelText, labelFont,
+                                          labelFill, placement, bufferColor,
+                                          bufferWidth)
+                })
+            ];
+    }
+}
+
 
 var style_Schneeschuhwanderwege_1 = function(feature, resolution){
-    var context = {
-        feature: feature,
-        variables: {}
-    };
-    
-    var labelText = ""; var value = feature.get("IsCHM");
-    var labelFont = "10px, sans-serif";
+
+    var labelText = "";
+    var value = feature.get("IsCHM");
+
+    var labelFont = "10px sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
-    var textAlign = "left";
-    var offsetX = 0;
-    var offsetY = 0;
     var placement = 'line';
-    if ("" !== null) {
-        labelText = String("");
-    }
-    
-    var style = categories_Schneeschuhwanderwege_1(feature, value, size, resolution, labelText,
-                            labelFont, labelFill, bufferColor,
-                            bufferWidth, placement);
+
+    var style = categories_Schneeschuhwanderwege_1(
+        feature,
+        value,
+        size,
+        resolution,
+        labelText,
+        labelFont,
+        labelFill,
+        bufferColor,
+        bufferWidth,
+        placement
+    );
 
     return style;
 };
--- styles/Winterwanderwege_3_style.js
+++ styles/Winterwanderwege_3_style.js
@@ -2,52 +2,91 @@ var size = 0;
 var placement = 'point';
 function categories_Winterwanderwege_3(feature, value, size, resolution, labelText,
                        labelFont, labelFill, bufferColor, bufferWidth,
-                       placement) {
-                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
-                switch(valueStr) {case '1':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,0,255,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 8}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    }),new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,170,220,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 6}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;
-case '0':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,100,220,0.6)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 6}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;}};
+                       placement)  {
+
+    var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+
+    // Zoomstufen
+    if (resolution >= 20) {
+        widthInner = 4;
+        widthOuter = 0;
+    }
+    else if (resolution >= 5) {
+        widthInner = 4;
+        widthOuter = 8;
+    }
+    else if (resolution >= 1) {
+        widthInner = 5;
+        widthOuter = 20;
+    }
+
+    switch(valueStr) {
+
+        case '1':
+
+            return [
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(224,68,194,0.7)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthInner
+                    }),
+                    text: createTextStyle(feature, resolution, labelText, labelFont,
+                                          labelFill, placement, bufferColor,
+                                          bufferWidth)
+                }),
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(165,28,113,0.4)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthOuter
+                    })
+                })
+            ];
+
+        case '0':
+
+            return [
+                new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: 'rgba(224,68,194,0.7)',
+                        lineCap: 'round',
+                        lineJoin: 'round',
+                        width: widthInner
+                    }),
+                    text: createTextStyle(feature, resolution, labelText, labelFont,
+                                          labelFill, placement, bufferColor,
+                                          bufferWidth)
+                })
+            ];
+    }
+}
 
 var style_Winterwanderwege_3 = function(feature, resolution){
-    var context = {
-        feature: feature,
-        variables: {}
-    };
-    
-    var labelText = ""; var value = feature.get("IsCHM");
-    var labelFont = "10px, sans-serif";
+
+    var labelText = "";
+    var value = feature.get("IsCHM");
+
+    var labelFont = "10px sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
-    var textAlign = "left";
-    var offsetX = 0;
-    var offsetY = 0;
     var placement = 'line';
-    if ("" !== null) {
-        labelText = String("");
-    }
     
-    var style = categories_Winterwanderwege_3(feature, value, size, resolution, labelText,
-                            labelFont, labelFill, bufferColor,
-                            bufferWidth, placement);
+    var style = categories_Winterwanderwege_3(
+        feature,
+        value,
+        size,
+        resolution,
+        labelText,
+        labelFont,
+        labelFill,
+        bufferColor,
+        bufferWidth,
+        placement
+    );
 
     return style;
 };
